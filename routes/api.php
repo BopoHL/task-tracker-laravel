b9ada4a5d4cf60b2f97eb690d4c3963c3f49f013
Leonid Voronin <127386605+BopoHL@users.noreply.github.com>
2024-03-11 12:18:23 +0500

    Added base CRUD routes for User, Project, Task, models

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/project', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{user_id}', [UserController::class, 'show']);
+Route::match(['put', 'patch'], '/users/{user_id}', [UserController::class, 'update']);
+Route::delete('users/{user_id}', [UserController::class, 'destroy']);
+
+Route::post('/projects', [ProjectController::class, 'store']);
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{project_id}', [ProjectController::class, 'show']);
+Route::match(['put', 'patch'], '/projects/{project_id}', [ProjectController::class, 'update']);
+Route::delete('projects/{project_id}', [ProjectController::class, 'destroy']);
+
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/{task_id}', [TaskController::class, 'show']);
+Route::match(['put', 'patch'], 'tasks/{task_id}', [TaskController::class, 'update']);
+Route::delete('tasks/{task_id}', [TaskController::class, 'destroy']);
+
+
+
